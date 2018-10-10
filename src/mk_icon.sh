31d93e0a435e3b41d0be383871153e6ea4ca2d33
Bert Goens <bertgoens@Berts-MacBook-Pro.local>
2018-10-10 07:05:12 +0200

    fix script directories

--- src/mk_icon.sh
+++ src/mk_icon.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 rm -rf bitmap
 rm -rf icons
-	
+
+echo "Create images"
 ./render_bitmap.sh
 
-      
+echo "Copy images"
+mkdir --parents --verbose ../icons/{128-48,256-96,384-144,512-192}
 
-mkdir -p icons/{128-48,256-96,384-144}
-cp -r bitmap/{big_128/*.png,small_48/*.png} icons/128-48
-cp -r bitmap/{big_256/*.png,small_96/*.png} icons/256-96 
-cp -r bitmap/{big_384/*.png,small_144/*.png} icons/384-144
+# I wonder if it would work better with --no-preserve=all
+cp -r ./bitmap/{big_128/*.png,small_48/*.png} ../icons/128-48
+cp -r ./bitmap/{big_256/*.png,small_96/*.png} ../icons/256-96 
+cp -r ./bitmap/{big_384/*.png,small_144/*.png} ../icons/384-144
+cp -r ./bitmap/{big_512/*.png,small_192/*.png} ../icons/512-192
 
-exit 0       
+exit 0
